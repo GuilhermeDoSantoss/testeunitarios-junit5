47ecf5372bf470ba4d0f8a745d2a82f5d3957fc7
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-02-19 17:48:22 -0300

    projeto criado

---
+++ src/main/java/com/guilhermesantos/cadastro_usuario_testeunit/CadastroUsuarioTesteunitApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermesantos.cadastro_usuario_testeunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroUsuarioTesteunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroUsuarioTesteunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermesantos/cadastro_usuario_testeunit/CadastroUsuarioTesteunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermesantos.cadastro_usuario_testeunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuarioTesteunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
